e3f844f37e883356b0652dd2968f5a22326edd13
Matheus Prado Agostinho <mat.matheus_pa@hotmail.com>
2021-11-17 17:31:16 -0300

    refactoring design
    migrating project to react with typescript

---
+++ src/App.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import styles from './styles/app.module.scss';
+
+function App() {
+  const [text, setText] = useState('');
+
+
+  function normal(){ //Transform text to normal
+    setText(toCapitalize(text)) 
+  }
+  function up(){ //Transform text to uppercase
+    setText(text.toUpperCase())
+  }
+  function low(){ //Transform text to lowercase
+    setText(text.toLowerCase())
+  }
+  function capitalize(){ //Transform text to first capital letters
+    setText(toCapitalize(text)) 
+  }
+  function clearTextBox(){ //Clean text box
+    setText('')
+  }
+
+  function copyText(){ //Copy text for transfer
+    navigator.clipboard.writeText(text)
+  }
+
+  function toCapitalize(str: string){ //Transform text to first capital letters
+    //split the above string into an array of strings 
+    //whenever a blank space is encountered
+    const arr = str.split(" ");
+
+    //loop through each element of the array and capitalize the first letter.
+    for (var i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+
+    }
+
+    //Join all the elements of the array back into a string 
+    //using a blankspace as a separator 
+    return arr.join(" ");
+  }
+
+  return (<>
+  <div className={styles.app}>
+    <div className={styles.content}>     
+      <textarea 
+        placeholder="Type or paste the text you want to convert here."
+        value={text}
+        onChange={(e) => setText(e.currentTarget.value)}
+      />
+      <button onClick={copyText} name='Copy'>
+        <img src="copy-icon.svg" alt="Copy"/>
+      </button>
+    </div>
+
+    <div className={styles.buttons}>
+      <button onClick={normal}>Normal</button>
+      <button onClick={up}>UPPERCASE</button>
+      <button onClick={low}>lowercase</button>
+      <button onClick={capitalize}>Capitalize</button>
+      <button onClick={clearTextBox}>Clear Text Box</button>
+    </div>
+  </div>
+  </>);
+}
+
+export default App;
