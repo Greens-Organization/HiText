096feca3c065e4b43a9e4d0d0920e2e3bec28543
Matheus P Agostinho <mat.matheus_pa@hotmail.com>
2021-12-03 11:15:58 -0400

    create menu, create UI new design

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,21 +1,47 @@
-import styles from '../styles/components/menu.module.scss';
+import React, { useState } from "react";
+import styles from "../styles/components/menu.module.scss";
 
-export function Menu(){
-  return(<>
-    <div className={styles.app}>
-      <div>
-        <img src="" alt="HiText!" />
+export function Menu() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  function changeTheme(state: boolean) {
+    setDarkMode(state);
+  }
+
+  return (
+    <>
+      <div className={styles.app}>
+        <header className={styles.content}>
+          <div className={styles.logo}>
+            <img src="" alt="HiText!" />
+          </div>
+          <div className={styles.chooseTheme}>
+            {darkMode ? (
+              <button onClick={() => changeTheme(false)}>
+                <img
+                  src="sun.svg"
+                  alt="Enable light mode"
+                  title="Enable light mode"
+                />
+              </button>
+            ) : (
+              <button onClick={() => changeTheme(true)}>
+                <img
+                  src="moon.svg"
+                  alt="Enable dark mode"
+                  title="Enable dark mode"
+                />
+              </button>
+            )}
+          </div>
+          <nav>
+            <a href="/">Home</a>
+            <a href="/">About</a>
+            <a href="/">Github</a>
+          </nav>
+        </header>
+        <main></main>
       </div>
-      <div>
-        <button>
-          <img src="" alt="Claro" />
-        </button>
-      </div>
-      <div>
-        <button>Home</button>
-        <button>About</button>
-        <button>Github</button>
-      </div>
-    </div>
-  </>)
+    </>
+  );
 }
