3a8c99a1bcf496d241e36280623c6ec3cf0ecbdf
Matheus P Agostinho <mat.matheus_pa@hotmail.com>
2021-12-09 10:45:21 -0400

    fix problmes router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Switch, Route } from "react-router";
 import { ChooseThemeProvider } from "./contexts/ChooseThemeContext";
 
@@ -8,12 +8,12 @@ import About from "./pages/About";
 export default function App() {
   return (
     <ChooseThemeProvider>
-      <Router>
+      <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
         </Switch>
-      </Router>
+      </BrowserRouter>
     </ChooseThemeProvider>
   );
 }
