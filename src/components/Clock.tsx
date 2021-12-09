a45c6d110ceb149c7a690bf7219bfc41ebdffda0
Matheus P Agostinho <mat.matheus_pa@hotmail.com>
2021-12-09 10:07:57 -0400

    styles clock

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -6,13 +6,25 @@ import styles from "../styles/components/clock.module.scss";
 
 //Componente
 export function Clock() {
+  const now = Date.now();
+  const [date, setDate] = useState();
+  const [hour, setHour] = useState();
+
+  useEffect(() => {
+    updateDateHour();
+  }, [now]);
+
+  function updateDateHour() {
+    setDate(dateFormat(now, "dd/mmmm/yyyy"));
+    setHour(dateFormat(now, "hh:M TT"));
+  }
+
   return (
     <>
       <div className={styles.app}>
-        <span>{dateFormat(Date.now(), "dd/mmmm/yyyy")}</span>
-        <span>{dateFormat(Date.now(), "hh:M TT")}</span>
+        <span>{date}</span>
+        <span>{hour}</span>
       </div>
-      ;
     </>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,20 @@
+//Components
 import { Menu } from "../components/Menu";
 import { TextArea } from "../components/TextArea";
+import { Clock } from "../components/Clock";
 
-export default function Home(){
-  return(<>
-    <Menu />
-    <TextArea />
-  </>)
+//Style
+import styles from "../styles/pages/home.module.scss";
+
+//Home page
+export default function Home() {
+  return (
+    <>
+      <Menu />
+      <div className={styles.content}>
+        <TextArea />
+        <Clock />
+      </div>
+    </>
+  );
 }
