f657ffb4261ecd1337b0adb610975bfd8892f8f7
Matheus P Agostinho <mat.matheus_pa@hotmail.com>
2021-12-10 08:55:23 -0400

    vai

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import dateFormat from "dateformat";
 
 //Style
-import styles from "../styles/components/clock.module.css";
 
 //Component clock
 export function Clock() {
@@ -17,9 +16,21 @@ export function Clock() {
 
   return (
     <>
-      <div className={styles.app}>
-        <span className={styles.span}>{date}</span>
-        <span className={styles.span}>{hour}</span>
+      <div className={"display: flex; flex-direction: column;"}>
+        <span
+          className={
+            "color: var(--placeholder-color); font-weight: bold; font-size: .75rem;"
+          }
+        >
+          {date}
+        </span>
+        <span
+          className={
+            "color: var(--placeholder-color); font-weight: bold; font-size: .75rem;"
+          }
+        >
+          {hour}
+        </span>
       </div>
     </>
   );
