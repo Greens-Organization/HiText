c1e325d89351c430a9686fab282caf15939b728f
AlphaLawless <alpha.vylly12@ufpi.edu.br>
2022-01-29 16:02:58 -0300

    change(src/): change name index into main

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-//Estilo
 import "./styles/globals.scss";
 
-//Componente
 ReactDOM.render(
   <React.StrictMode>
     <App />
