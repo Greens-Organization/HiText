fb1df45ba85b39a9e992110e83bd6bb4a0611471
AlphaLawless <alpha.vylly12@ufpi.edu.br>
2022-01-30 00:37:53 -0300

    update(pages): removed comments and export with lambda function

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,9 @@
-//Components
 import { Menu } from "../components/Menu";
 import { TextArea } from "../components/TextArea";
 import { Clock } from "../components/Clock";
 
-//Style
 import styles from "../styles/pages/home.module.scss";
 
-//Home page
 export default function Home() {
   return (
     <>
