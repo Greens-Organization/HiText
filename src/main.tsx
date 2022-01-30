2749f6e4ed5b8378df2e5dc5eaf57ef0cf9cc49b
AlphaLawless <alpha.vylly12@ufpi.edu.br>
2022-01-30 00:40:54 -0300

    update(styles): change name in import global styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-import "./styles/globals.scss";
+import "./styles/global.scss";
 
 ReactDOM.render(
   <React.StrictMode>
