48c9171538314c2d8bc1883736a3304d46cf630e
AlphaLawless <alpha.vylly12@ufpi.edu.br>
2022-02-02 21:44:51 -0300

    refactor(menu): refactoring process

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,50 +1,26 @@
-import { useTheme } from "../hooks/useChooseTheme";
-import { Link } from "react-router-dom";
-
 import styles from "../styles/components/menu.module.scss";
 
 export const Menu = () => {
-  const { darkOn, enableDarkMode, enableLightMode } = useTheme();
-
   return (
-    <>
-      <div className={styles.app}>
-        <header className={styles.content}>
-          <div className={styles.logo}>
-            {darkOn ? (
-              <img src="/images/dark-logo.svg" alt="HiText!" />
-            ) : (
-              <img src="/images/light-logo.svg" alt="HiText!" />
-            )}
-          </div>
-          <div className={styles.chooseTheme}>
-            {darkOn ? (
-              <button onClick={enableLightMode}>
-                <img
-                  src="/images/sun.svg"
-                  alt="Enable light mode"
-                  title="Enable light mode"
-                />
-              </button>
-            ) : (
-              <button onClick={enableDarkMode}>
-                <img
-                  src="/images/moon.svg"
-                  alt="Enable dark mode"
-                  title="Enable dark mode"
-                />
-              </button>
-            )}
-          </div>
-          <nav>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <a href="https://github.com/Greens-Organization/HiText/tree/main">
-              GitHub
-            </a>
-          </nav>
-        </header>
-      </div>
-    </>
+    <div className={styles.app}>
+      <header className={styles.content}>
+        <div className={styles.logo}>
+          <img src="/images/dark-logo.svg" alt="HiText Logo" />
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <a
+                href="https://github.com/Greens-Organization/HiText"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                GitHub
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
   );
 };
