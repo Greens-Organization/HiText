796a2ac508c396fa2fee88d7ee48b2ea80a994c0
AlphaLawless <alpha.vylly12@ufpi.edu.br>
2022-02-02 21:44:11 -0300

    refactor(clock): refactoring process

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,14 +1,3 @@
-import { parseISO, format } from "date-fns";
-
 export const Clock = () => {
-  const now = Date.now().toString();
-
-  const date = parseISO(now);
-
-  return (
-    <div>
-      <time dateTime={now}>{format(date, "dd/mmmm/yyyy")}</time>
-      <time dateTime={now}>{format(date, "hh:mm a..aa")}</time>
-    </div>
-  );
+  return <div>Hello Time</div>;
 };
