37edef1aec5ed7b1d578f9bb34010ec043d78307
AlphaLawless <alpha.vylly12@ufpi.edu.br>
2022-02-04 19:54:02 -0300

    add(provider): ChooseTheme Provider in all app

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { ChooseThemeProvider } from "./contexts/ChooseThemeContext";
 
 import "./styles/global.scss";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChooseThemeProvider>
+      <App />
+    </ChooseThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
