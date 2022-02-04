24e83aaa10af2c068fb6c16b43f7276a7e266b07
AlphaLawless <alpha.vylly12@ufpi.edu.br>
2022-02-03 23:51:20 -0300

    feat: active button in current page

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,11 @@
+import { Link } from "react-router-dom";
 import styles from "../styles/components/menu.module.scss";
 
-export const Menu = () => {
+interface Props {
+  home?: boolean;
+}
+
+export const Menu = (props: Props) => {
   return (
     <div className={styles.app}>
       <header className={styles.content}>
@@ -9,6 +14,25 @@ export const Menu = () => {
         </div>
         <nav>
           <ul>
+            {props.home ? (
+              <>
+                <li className={styles.active}>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li className={styles.active}>
+                  <Link to="/about">About</Link>
+                </li>
+              </>
+            )}
             <li>
               <a
                 href="https://github.com/Greens-Organization/HiText"
