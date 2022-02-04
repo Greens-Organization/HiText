a152eab3c5d4a8223052e03dd20e35601b9f05e7
AlphaLawless <alpha.vylly12@ufpi.edu.br>
2022-02-03 23:48:43 -0300

    update(ThemeContext): add ThemeContext in aplication

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { ThemeContextWrapper } from "./contexts/theme/ThemeContextWrapper";
 
 import "./styles/global.scss";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ThemeContextWrapper>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeContextWrapper>,
   document.getElementById("root")
 );
