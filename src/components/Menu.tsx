0c0e57f54fe2e2dd80b2b9b627392ee43d6afc1a
AlphaLawless <alpha.vylly12@ufpi.edu.br>
2022-02-04 17:44:20 -0300

    feat(context): start of color scheme change

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,20 +1,40 @@
 import { Link } from "react-router-dom";
+import { useTheme } from "../hooks/useChooseTheme";
 import styles from "../styles/components/menu.module.scss";
 
 interface Props {
   home?: boolean;
 }
 
-export const Menu = (props: Props) => {
+export const Menu = ({ home }: Props) => {
+  const { theme, switchTheme } = useTheme();
   return (
     <div className={styles.app}>
       <header className={styles.content}>
         <div className={styles.logo}>
-          <img src="/images/dark-logo.svg" alt="HiText Logo" />
+          {theme ? (
+            <img
+              src="/images/light-logo.svg"
+              alt="HiText Logo for Light Mode"
+            />
+          ) : (
+            <img src="/images/dark-logo.svg" alt="HiText Logo for Dark Mode" />
+          )}
+        </div>
+        <div className={styles.chooseTheme}>
+          {theme ? (
+            <button onClick={switchTheme}>
+              <img src="/images/moon.svg" alt="" />
+            </button>
+          ) : (
+            <button onClick={switchTheme}>
+              <img src="/images/sun.svg" alt="" />
+            </button>
+          )}
         </div>
         <nav>
           <ul>
-            {props.home ? (
+            {home ? (
               <>
                 <li className={styles.active}>
                   <Link to="/">Home</Link>
