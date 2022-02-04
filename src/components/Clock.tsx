36c3c006096669f2d470ad8c8602d5a4692d6596
AlphaLawless <alpha.vylly12@ufpi.edu.br>
2022-02-03 23:46:58 -0300

    feat(clock): created functional clock

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,3 +1,40 @@
+import { useEffect, useState } from "react";
+import styles from "../styles/components/clock.module.scss";
+
 export const Clock = () => {
-  return <div>Hello Time</div>;
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    let secDate = setInterval(() => {
+      setCurrentDate(new Date());
+    }, 1000);
+    return () => clearInterval(secDate);
+  }, []);
+
+  useEffect(() => {
+    let secTime = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+    return () => clearInterval(secTime);
+  });
+
+  return (
+    <div className={styles.container}>
+      <time>
+        {currentDate.toLocaleString("pt-br", {
+          year: "numeric",
+          month: "2-digit",
+          day: "2-digit"
+        })}
+      </time>
+      <time>
+        {currentTime.toLocaleString("pt-br", {
+          hour: "numeric",
+          minute: "numeric",
+          hour12: true
+        })}
+      </time>
+    </div>
+  );
 };
