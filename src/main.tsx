1f1f719b7fcd694535524f8f7063fa20ae759b8d
AlphaLawless <alpha.vylly12@ufpi.edu.br>
2022-02-04 17:45:35 -0300

    update(provider): remove choosethemeprovider from main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { ThemeContextWrapper } from "./contexts/theme/ThemeContextWrapper";
 
 import "./styles/global.scss";
 
 ReactDOM.render(
-  <ThemeContextWrapper>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ThemeContextWrapper>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById("root")
 );
